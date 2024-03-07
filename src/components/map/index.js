ef6a1ed2276e6d73caf0b0638ea46758556b1be9
Igor Jair Laurindo Ribeiro <igorjairlaurindoribeiro@gmail.com>
2024-03-07 18:30:35 +0000

    more fix

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -15,20 +15,20 @@ const bounds = [
 
 function Mapa() {
   const mapRef = useRef();
+  const history = useHistory();
+
   useEffect(() => {
-    console.log("Deu certo");
     const hashParams = new URLSearchParams(window.location.hash.slice(1)); // Obtém os parâmetros do fragmento da URL
     const searchParam = hashParams.get('search'); // Obtém o valor do parâmetro 'search'
   
     if (searchParam) {
-      // Realize a função com o parâmetro 'search'
       console.log('Parâmetro de busca:', searchParam);
       // Aqui você pode realizar qualquer função desejada com o parâmetro 'search'
   
-      // Redirecionar para a URL desejada com o parâmetro de busca
-      window.location.href = `/Meu_IFC_Light/search:${searchParam}`;
+      // Redirecionar para a URL desejada com o parâmetro de busca sem recarregar a página
+      history.push(`/Meu_IFC_Light/search:${searchParam}`);
     }
-  }, []);
+  }, [history]);
 
   useEffect(() => {
     const { current = {} } = mapRef;
